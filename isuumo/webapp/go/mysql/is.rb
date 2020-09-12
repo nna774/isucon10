0f6c3eb0eb09fd51d32bad08c1e5ff1cb99b783a
Kugayama Nana <nonamea774@gmail.com>
2020-09-12 09:29:19 +0000

    add i

--- isuumo/webapp/go/mysql/is.rb
+++ isuumo/webapp/go/mysql/is.rb
@@ -5,7 +5,30 @@ arr = ["ゲーミングチェア",
 
 cmd = 'sed -i '
 arr.each_with_index do |v,i|
-    cmd += "-e \"s/'#{v}'/'#{v}', #{i}/g\" "
+    cmd += "-e \"s/'#{v}'/'#{v}', '#{i}'/g\" "
 end
 
+cmd += ' mysql/2_DummyChairData.sql'
+
+puts cmd
+
+arr = [    "黒",
+"白",
+"赤",
+"青",
+"緑",
+"黄",
+"紫",
+"ピンク",
+"オレンジ",
+"水色",
+"ネイビー",
+"ベージュ"]
+
+cmd = 'sed -i '
+arr.each_with_index do |v,i|
+    cmd += "-e \"s/'#{v}'/'#{v}', '#{i}'/g\" "
+end
+
+cmd += ' mysql/2_DummyChairData.sql'
 puts cmd
